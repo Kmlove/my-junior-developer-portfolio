88c82d84a95d1df7ceac903141b6bb2cd2b2f0b3
Kim Love <kimberlymlove15@gmail.com>
2023-12-08 17:24:56 -0500

    Updated fetch call to API in Blog component for deployment

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -12,7 +12,7 @@ function Blog() {
   const [ mediumData, setMediumData ] = useState()
 
   useEffect(() => {
-    fetch('/medium_info')
+    fetch('https://medium-blog-api.onrender.com/medium_info')
     .then(res => res.json())
     .then(data => {
       setMediumData(data)

1ece100b866b58f9553f5460649afca4928d1cc9
Kim Love <kimberlymlove15@gmail.com>
2024-06-27 02:02:52 -0400

    Created a reusableComponent for the details page

---
+++ src/components/ProjectDetails/ReusableDetailContainer.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import "../../css/projectDetailStyles.scss";
+import KLlogo from "../../images/Kimberly-Love-Logo-Color-Zoomed.png";
+import LargeDetailNavBar from "./LargeDetailNavBar";
+import SmallDetailNavBar from "./SmallDetailNavBar";
+
+function ReusableDetailContainer({ children, detailPageHeader }) {
+  const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setViewportWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <div
+      className="detail-page"
+      style={{ height: viewportWidth > 630 ? "100vh" : "100%" }}
+    >
+      {viewportWidth > 630 ? (
+        <LargeDetailNavBar KLlogo={KLlogo} />
+      ) : (
+        <SmallDetailNavBar KLlogo={KLlogo} />
+      )}
+
+      <div className="detail-container">
+        <h1 className="detail-header">{detailPageHeader}</h1>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default ReusableDetailContainer;

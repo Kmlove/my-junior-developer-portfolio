bcc8f0e8f0779f5d1f0fcc39a928b52b4bed43f5
Kim Love <kimberlymlove15@gmail.com>
2024-06-27 12:32:16 -0400

    Added content to the details section for this project

--- src/components/ProjectDetails/FitConnectDetails.jsx
+++ src/components/ProjectDetails/FitConnectDetails.jsx
@@ -1,7 +1,41 @@
 import React from "react";
 
 function FitConnectDetails() {
-  return <div>FitConnectDetails</div>;
+  return (
+    <>
+      <h2>Frontend</h2>
+      <ul>
+        <li>
+          Integrated frontend form validations, utilizing React state management for
+          real-time UI error messages based on user input
+        </li>
+        <li>
+          Implemented an efficient filter feature with React and CSS to update
+          the UI in the Posts section on user interaction
+        </li>
+        <li>
+          Utilized advanced CSS techniques such as Flexbox to optimize layout
+          and design in FitConnect
+        </li>
+        <li>
+          Collaborated with a team to design and integrate fitness logging and
+          social media front-end features with React
+        </li>
+      </ul>
+
+      <h2>Backend</h2>
+      <ul>
+        <li>
+          Developed backend user-centric Posts functionality, enabling creation,
+          editing, and deletion of workout-related posts
+        </li>
+        <li>
+          Collaborated with a team and managed code across the entire software
+          development lifecycle using Git
+        </li>
+      </ul>
+    </>
+  );
 }
 
 export default FitConnectDetails;
--- src/components/ProjectDetails/FlatStopDetils.jsx
+++ src/components/ProjectDetails/FlatStopDetils.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 function FlatStopDetils() {
-  return <div>FlatStopDetils</div>;
+  return (
+    <>
+      <h2>Backend</h2>
+      <ul>
+        <li>
+          Developed a Python-based Command Line Interface (CLI) application,
+          creating an immersive virtual game store
+        </li>
+        <li>
+          Wrote SQL queries to seamlessly map Python classes to a relational
+          database (SQLite) to perform CRUD
+        </li>
+        <li>
+          Employed conditional statements and loops to create interactive and
+          user-friendly features within the CLI interface
+        </li>
+        <li>
+          Leveraged object-oriented programming principles in Python to design
+          modular and maintainable codebase
+        </li>
+      </ul>
+    </>
+  );
 }
 
 export default FlatStopDetils;

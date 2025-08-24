2bebe6eb46b54f243045782cf8ed311f13c4a829
Kim Love <kimberlymlove15@gmail.com>
2025-08-24 15:14:39 -0400

    Added useEffect as a workaround for scrolling to an anchor point

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import MainPage from './components/MainPage';
 import { Bounce, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
+import { useEffect } from 'react';
 
 function App() {
+
+  // Add this in a useEffect on the target page
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const el = document.querySelector(hash);
+      if (el) {
+        el.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, []);
+
   return (
     <div id="app">
       <MainPage />

76df6136dc43692ba9b8c1e0cede69f133ea67c6
Kim Love <kimberlymlove15@gmail.com>
2025-08-24 15:16:46 -0400

    Made Logo and home button navigation linked but hid the underline on logo

--- src/components/LargeNavbar.js
+++ src/components/LargeNavbar.js
@@ -1,38 +1,55 @@
-import React from 'react'
-import { Navbar, Nav} from 'react-bootstrap'
-import '../css/navBarStyles.scss'
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import "../css/navBarStyles.scss";
 
-function LargeNavbar({KLlogo}) {
-
-    return (
-        <Navbar id="large-nav">
-            <a href='#home'>
-                <div id="KLlogo">
-                    <img src={KLlogo} alt='Kimberly Love Logo' />
-                </div>
-            </a>
-            <Nav variant="underline">
-                <Nav.Item>
-                    <Nav.Link eventKey="#home" href="#home">Home</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="#aboutMe" href="#aboutMe">About Me</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="#skills" href="#skills">Skills</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="#projects" href="#projects">Projects</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="#blogSection" href="#blogSection">Blog</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="#contact" href="#contact">Contact</Nav.Link>
-                </Nav.Item>
-            </Nav>
-        </Navbar>
-    )
+function LargeNavbar({ KLlogo }) {
+  return (
+    <Navbar id="large-nav">
+      <Nav variant="underline" className="nav-scoped">
+        <div clasName="logo-wrapper">
+        <Nav.Item>
+          <Nav.Link eventKey="#home" href="#home" style={{borderBottomColor: 'transparent'}}>
+            <div id="KLlogo">
+              <img src={KLlogo} alt="Kimberly Love Logo" />
+            </div>
+          </Nav.Link>
+        </Nav.Item>
+        </div>
+        <div className='navlinks-wrapper'>
+        <Nav.Item>
+          <Nav.Link eventKey="#home" href="#home">
+            Home
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="#aboutMe" href="#aboutMe">
+            About Me
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="#skills" href="#skills">
+            Skills
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="#projects" href="#projects">
+            Projects
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="#blogSection" href="#blogSection">
+            Blog
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="#contact" href="#contact">
+            Contact
+          </Nav.Link>
+        </Nav.Item>
+        </div>
+      </Nav>
+    </Navbar>
+  );
 }
 
-export default LargeNavbar
+export default LargeNavbar;
